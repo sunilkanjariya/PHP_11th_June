2e6bed695c59424952f849ad2a46464045ef8013
shreetuition <info@shreetuitioncenter.com>
2021-07-05 12:38:01 +0530

    Task Done

--- index.php
+++ index.php
@@ -1,7 +1,45 @@
 <?php 
-	
-	echo "Hello";
+	/*echo "Hello";
+	echo "Test"; */
 
-	echo "Test";
+	// Variables are the names you give to computer memory locations which are used to store values in a computer program.
 
+	// For example, assume you want to store two values 10 and 20 in your program and at a later stage, you want to use these two values. Let's see how you will do it. Here are the following three simple steps −
+
+	// Create variables with appropriate names.
+	// Store your values in those two variables.
+	// Retrieve and use the stored values from the variables.
+
+	require "tesasdt.php";
+	echo $name12_ = "Sharvan";
+	$NAME = "Kumar";
+
+	echo $name;
+
+	echo "<h1>Hello This is Kumar</h1>";
+
+	echo "<br>";
 ?>
+
+<!-- <a href="test.php">Test Page</a>
+
+<a href="index.php">Home</a> -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<style type="text/css">
+		h1 {
+			font-size: <?php echo "50px"; ?>;
+		}
+	</style>
+</head>
+<body>
+	<h1><?php echo "Hello This is New Line"; ?></h1>
+
+	<a href="<?php echo "https://www.tops-int.com/java-training-course-online/"; ?>"><?php echo "Java"; ?></a>
+
+	<h2><?php echo $name; ?></h2>
+</body>
+</html>
