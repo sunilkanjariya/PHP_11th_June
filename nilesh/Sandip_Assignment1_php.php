288421c0769fc6f1217d1a90446978eed936560d
shreetuition <info@shreetuitioncenter.com>
2021-07-09 12:20:49 +0530

    Done

--- nilesh/Sandip_Assignment1_php.php
+++ nilesh/Sandip_Assignment1_php.php
@@ -1,178 +1,48 @@
-<?php
-$num1 = $_POST['number1'];
-$num2=$_POST['number2'];
-$submit = $_POST['submit'];
-?>
-
-<?php
-    if(isset($submit)){
-        $num1;
-        if( $num1 == ''){
-            $error1 =" ";
-           
-       } 
-    }
-?>
-
-<?php
-    if(isset($submit)){
-        $num2;
-        if( $num2 == ''){
-            $error2 =" ";
-  
+<?php 
+    
+   // $_POST['submit'];
+    if (isset($_POST['submit'])) {
+       $number = $_POST['number'];
+       if($number=='') {
+            $error =  "Number is Blank";
+       } else {
+            if ($number%2==0) {
+                $result =  "<h1 style='color: green; text-align: center;'>Even number</h1>";
+            } else {
+                $result =  "<h1 style='color: red; text-align: center;'>Odd number</h1>";
+            }
        }
     }
+
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Odd-Even-Table</title>
-    <style>
-        table{
-            border:1px solid blue;
-            width:50vw;
-            margin:auto;
-        }
-        table td,th{
-            padding:10px;
-            border:1px solid black
-        }
-        h1{
-            text-align:center;
-        }
-        h3{
-            text-align:center;
-            color:blue;
-        }
-        span{
-            color:red;
-            font-size:15px;
-
-        }
-    </style>
+    <meta charset="utf-8">
+    <title></title>
 </head>
 <body>
-    <form action="" method="post">
-        <table>
-            <tr>
-                <th colspan="2">Number Analyzer</th>
-            </tr>
-            <tr>
-                <td>Enter First Number:</td>
-                <td><input type="text" name="number1">
-                 <span>
-                    <?php
-                        if(isset($error1)){
-                        echo $error1 ="Enter First Number";
-                        }
-                    ?>
-                </span>
-                
-                </td>
-            </tr>
-            <tr>
-                <td>Enter Second number:</td>
-                <td><input type="text" name="number2">
-                 <span>
-                    <?php
-                        if(isset($error2)){
-                        echo $error2 ="Enter Second Number";
-                        }
-                    ?>
-                </span>
-                
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:center" colspan="2"><input type="submit" name="submit">
-                
-               
-                
-                </td>
-            </tr>
-        </table>
+
+    <form method="post">
+        <label>Enter The Number: </label>
+        <input type="text" name="number">
+
+        <?php 
+            if (isset($error)) {
+                ?>
+                <span style="color: red;"><?php echo $error; ?></span>
+                <?php
+            }
+        ?>
+        <br><br>
+        <input type="submit" name="submit">
     </form>
+    <?php 
 
-<?php
-    if(isset($submit)){
-
-        if($num1 > $num2){
-            ?>  
-            <h1 style="color:blue"> <?php  echo "First Number $num1 is greater than Second Number $num2" ;  ?> </h1>
-            <?php
-        }
-        else if($num1 < $num2){
-            ?>
-            <h1 style="color:blue"><?php   echo "Second Number $num2 is greater than First Nember $num1"  ; ?>  </h1>
-            <?php
+        if (isset($result)) {
+            echo $result;
         }
 
-        else if ($num1 = ' '  && $num2= ' '){
-            ?>
-            <h1 style="color:blue"><?php   echo "Both Nunbers Are Not Set"  ; ?>  </h1>
-            <?php
-        }
-
-        else {
-            ?>
-            <h1 style="color:blue"><?php   echo "Both Nunbers Are Same"  ; ?>  </h1>
-            <?php
-        }
-    }
-    else {
-       echo " Number Not Available";
-    }
-?>
-
-
-
-<?php
-    if(isset($error1)){
-     $error1;
-    }
-    else{
-        if($num1 == 0){
-            echo "<h1 style=color:blue>". "Your Number Is Zero"."</h1>";
-        }
-        else{
-            if($num1 %2 == 0 ){
-                echo "<h1 style=color:green>"."$num1 is even"."</h1>";
-            }
-            else if($num1 %2 != 0){
-                echo "<h1 style=color:red>"."$num1 is odd"."</h1>";
-            }
-        }
-    }
-?>
-
-
-<?php
-   if(isset($error2)){
-    $error2;
-  }
-  else{
-
-        if($num2 == 0){
-            echo "<h1 style=color:blue>"."Your Number Is Zero"."</h1>";
-        }
-        else{
-            if($num2 %2 == 0 ){
-                echo "<h1 style=color:green>"."$num2 is even"."</h1>";
-            }
-            else if($num2 %2 != 0){
-                echo "<h1 style=color:red>"."$num2 is odd"."</h1>";
-            }
-        }
-    }
-?>
-
-
-
-
+    ?>
 </body>
 </html>
--- test.php
+++ test.php
@@ -1,6 +1,15 @@
-<?php 
-	
-
-	echo "Test Page";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<i class="fa fa-battery-full" aria-hidden="true"></i>
+	<i class="fa fa-address-book"></i>
+	<i class="fa fa-address-book" style="font-size:24px"></i>
+	<i class="fa fa-address-book" style="font-size:36px"></i>
+	<i class="fa fa-address-book" style="font-size:48px;color:red"></i>
+</body>
+</html>
